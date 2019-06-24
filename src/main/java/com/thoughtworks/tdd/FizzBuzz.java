7059c4aa527bc17df1f754dfc3f707585f8ed209
xsu <xsu@thoughtworks.com>
2019-06-24 11:24:52 +0800

    [green] fixed main loop test.

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -3,7 +3,10 @@ package com.thoughtworks.tdd;
 public class FizzBuzz {
 
     public void action() {
-
+        for( int i = 0; i < 120; i++) {
+            String result = fizzBuzz(i+1);
+            System.out.println(result);
+        }
     }
 
     public String fizzBuzz(int id) {
