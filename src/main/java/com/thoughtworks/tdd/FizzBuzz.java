a624c2c2933eee8b7896a96460f6b83d3d52de4d
xsu <xsu@thoughtworks.com>
2019-06-24 15:52:38 +0800

    节约时间，一步到位。

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -3,14 +3,28 @@ package com.thoughtworks.tdd;
 public class FizzBuzz {
 
     public void action() {
-        for( int i = 0; i < 120; i++) {
-            String result = fizzBuzz(i+1);
+        for (int i = 0; i < 120; i++) {
+            String result = fizzBuzz(i + 1);
             System.out.println(result);
         }
     }
 
     public String fizzBuzz(int id) {
-        return "" + id;
+        if (id % 3 != 0 && id % 5 != 0 && id % 7 != 0) {
+            return "" + id;
+        } else {
+            String result = "";
+            if (id % 3 == 0) {
+                result += "Fizz";
+            }
+            if (id % 5 == 0) {
+                result += "Buzz";
+            }
+            if (id % 7 == 0) {
+                result += "Whizz";
+            }
+            return result;
+        }
     }
 
 }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -26,4 +26,39 @@ class FizzBuzzTest {
     void should_return_id_if_not_divide_by_3_5_7() {
         assertEquals("2", this.fizzBuzz.fizzBuzz(2));
     }
+
+    @Test
+    void should_return_Fizz_if_divide_by_3_but_not_divide_by_5_7() {
+        assertEquals("Fizz", this.fizzBuzz.fizzBuzz(6));
+    }
+
+    @Test
+    void should_return_Fizz_if_divide_by_5_but_not_divide_by_3_7() {
+        assertEquals("Buzz", this.fizzBuzz.fizzBuzz(10));
+    }
+
+    @Test
+    void should_return_Fizz_if_divide_by_7_but_not_divide_by_3_5() {
+        assertEquals("Whizz", this.fizzBuzz.fizzBuzz(14));
+    }
+
+    @Test
+    void should_return_Fizz_if_divide_by_3_5_but_not_divide_by_7() {
+        assertEquals("FizzBuzz", this.fizzBuzz.fizzBuzz(15));
+    }
+
+    @Test
+    void should_return_Fizz_if_divide_by_3_7_but_not_divide_by_5() {
+        assertEquals("FizzWhizz", this.fizzBuzz.fizzBuzz(21));
+    }
+
+    @Test
+    void should_return_Fizz_if_divide_by_5_7_but_not_divide_by_3() {
+        assertEquals("BuzzWhizz", this.fizzBuzz.fizzBuzz(35));
+    }
+
+    @Test
+    void should_return_Fizz_if_divide_by_3_5_7() {
+        assertEquals("FizzBuzzWhizz", this.fizzBuzz.fizzBuzz(105));
+    }
 }
